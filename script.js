35375fb0523fc664b97b0e7024878b4988ee71c5
AlbertT4 <84191428+ATedeschi247@users.noreply.github.com>
2022-11-18 16:03:16 +0000

    started secret konami code implementation.

---
+++ script.js
@@ -0,0 +1,6 @@
+const konamiCorrect = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+document.addEventListener(keyup, konamiCode);
+
+function konamiCode(event) {
+  
+}
