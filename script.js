0c1521907d99a12018dbc2edea3fbabd5bd9350f
AlbertT4 <84191428+ATedeschi247@users.noreply.github.com>
2022-11-22 13:25:31 +0000

    Added konami code implementation.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+let i = 0;
+let konamiCurrent = [];
 const konamiCorrect = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
-document.addEventListener(keyup, konamiCode);
+let konamiReverse = konamiCorrect;
+konamiReverse.reverse();
+document.addEventListener('keyup', konamiCode);
 
 function konamiCode(event) {
-  
+  if(event.key == konamiCorrect[i]) {
+    i++;
+    konamiCurrent.push(event.key);
+  } else {
+    konamiCurrent = [];
+    i = 0;
+  }
+  if(i == 10) {
+    window.location.href = 'https://youtu.be/hw_pwSKDGos';
+  }
 }
