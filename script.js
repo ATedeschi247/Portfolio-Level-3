1598af63b080d125699fb69a9963a28d53148494
AlbertT4 <84191428+ATedeschi247@users.noreply.github.com>
2022-11-22 13:54:23 +0000

    Fixed regular konami code and added reverse konami code easter egg.

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 let i = 0;
+let j = 0;
 let konamiCurrent = [];
-const konamiCorrect = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
-let konamiReverse = konamiCorrect;
-konamiReverse.reverse();
+let konamiCorrect = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+let konamiReverse = ['a', 'b', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowDown','ArrowDown', 'ArrowUp', 'ArrowUp'];
 document.addEventListener('keyup', konamiCode);
 
 function konamiCode(event) {
-  if(event.key == konamiCorrect[i]) {
+  if((event.key == konamiCorrect[i]) && j==0) {
     i++;
+    konamiCurrent.push(event.key);
+  } else if((event.key == konamiReverse[j]) && i==0) {
+    j++;
     konamiCurrent.push(event.key);
   } else {
     konamiCurrent = [];
     i = 0;
+    j = 0;
   }
   if(i == 10) {
+    window.location.href = 'https://youtu.be/dQw4w9WgXcQ';
+  }
+  if(j == 10) {
     window.location.href = 'https://youtu.be/hw_pwSKDGos';
   }
 }
